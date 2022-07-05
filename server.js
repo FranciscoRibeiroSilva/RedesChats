e606dab129c5ca1914bdf8e23db17f92ece51e57
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2022-07-05 19:36:07 -0300

    cnds socktet.io and http

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { dirname } from 'path';
+const express = require('express')
 const path = require('path');
+const { Socket } = require('socket.io');
 
 const app = express();
 
@@ -8,12 +8,16 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(dirname, 'public'));
+app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('views engine', 'html');
 
 app.use('/', (req, res)=>{
     res.render('index.html');
-})
+});
+
+io.on('connection', socket =>{
+    console.log('Socket conectado: ${socket.id}')
+});
 
 server.listen(3000);
