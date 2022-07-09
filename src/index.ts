9e870ca38260822cce9a7c5604b2613be8466a79
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2022-07-09 13:56:12 -0300

    ssl local

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express  from "express";
 import socketio from 'socket.io';
-import http from 'http';
-import path from "path";
+import http from 'http'
+import https from 'https'
+import path from "path"
+import fs from 'fs'
 
 const app = express();
-const httpServer = http.createServer(app);
+
+const httpServer = https.createServer({
+    key: fs.readFileSync('./.cert/localhost-private.pem'),
+    cert: fs.readFileSync('./.cert/localhost-cert.pem'),
+    //passphrase: '04061998666'
+},app);
+
+//const httpServer = http.createServer(app);
 const io = new socketio.Server(httpServer);
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
@@ -17,13 +26,17 @@ app.use('/', (req, res)=>{
     res.render('index.html')
 })
 
-let messagens = [];
+let messagens: any[] = [];
 
 io.on('connection', (socket)=>{
     console.log(`new connection: ${socket.id}`)
 
+    socket.emit('previousMessage', messagens);
+
     socket.on('sendMessage', data =>{
         messagens.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+
     })
 
 })
